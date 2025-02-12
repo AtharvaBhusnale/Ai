938f21a99e22dfcdef2069d4fc991422ca5b6cbd
AtharvaBhusnale <143188526+AtharvaBhusnale@users.noreply.github.com>
2025-02-12 11:21:45 +0530

    Update Selectionsort.cpp

--- Selectionsort.cpp
+++ Selectionsort.cpp
@@ -3,7 +3,7 @@
 using namespace std ;
 class Selection
 {
-	int arr[100] , size;
+	int arr[100] , size; // declare array and its size
 
 	public :
 
@@ -11,12 +11,12 @@ class Selection
 	{
 		int val;
 		cout<<"Enter the size of array :: ";
-		cin>>size ;
+		cin>>size ; // Initialize the size of the array
 		arr[size];
 		for (int i=0 ; i<size ; i++)
 		{
 			printf("Enter the value at %d :",i);
-			scanf("%d" , &val);
+			scanf("%d" , &val); // input value for each element 
 			arr[i]=val;
 		}
 	}
@@ -26,25 +26,25 @@ class Selection
 		for (int i=0 ; i<size ; i++)
 		{
 			
-			cout<<arr[i]<<" ";
+			cout<<arr[i]<<" "; // display the array
 		}
 	}
 
 	void sel_sort()
 	{
-		int min , temp;
+		int min , temp; // declare min element and temporary element
 
 		for(int i=0 ; i<size-1 ; i++)
 		{
-			min=i;
+			min=i; // initlaize i as the min element
 			for (int j = i+1; j < size; j++)
 			{
-				if (arr[min]>=arr[j])
-					min=j ;
+				if (arr[min]>=arr[j]) // if min element is greater the jth element  
+					min=j ; // assign j as min element
 			}
-			if(min != i)
+			if(min != i) // swapping the two variable
 			{
-				temp=arr[min];
+				temp=arr[min]; 
 				arr[min]=arr[i];
 				arr[i]=temp;
 			}
